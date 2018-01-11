f901e76b20af8ffb67acd3ab9dfa48e640ccdf0f
carlosdortiz <33131163+carlosdortiz@users.noreply.github.com>
2018-01-10 21:51:34 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,43 +1,96 @@
+<?php 
+
+     $errors = "";
+   //aqui se conecta con la base de datos
+  
+   $db = mysqli_connect('localhost','root', ' ', 'todo' );
+
+   if (isset($_POST['submit'])) {
+   	    $task = $_POST['task'];
+    	if (empty($task)) {
+
+   		$errors = " debes hacer la tarea ";
+       }else{
+
+   			mysqli_query($db,"INSERT INTO tasks (task) VALUES ('$task')");
+   	        header('location: index.php');	
+   	 
+     	} 
+
+     	}
+     	//borrar tarea
+     	if (insset($_GET['del_task'])) {
+     		$id = $_GET ['del_task'];
+     		mysqli_query($db, "DELETE FROM tasks WHERE id=$id");
+     		header('location: index.php'); 
+     	}
+
+   
+   }
+   if (isset($_GET['del_task'])) {
+   	$id = $_GET ['del_task'];
+   	mysqli_query($db, "DELETE FROM tasks WHERE id =$id");
+   	header('location: inde.php');
+
+   }
+
+   $tasks = mysqli_query($db, "SELECT * FROM tasks");
+
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Todo List</title>
+	<title>Todo list</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<form action="Registro.php" method="POST">
-	Clave: <input type="text" name="txtClave"> <br/>
-	Tarea: <input type="text" name="txtTarea"> <br/>
-	<input type="submit" value="Registrar" name="btnRegistrar">
+	<div class="heading">
+		<h2>Todo list</h2>
+	</div>
 
 
-</form>
-<p>..............................................................</p>
+	<form method="POST" action="index.php">
+     <?php if (isset($errors)) {?>
+     	<p><?php  echo $errors; ?></p> 
 
-	<form action="eliminar.php" method="POST">
-		Clave:  <input type="text" name="txtClave"> <br/>
-		<input type="submit" value = "Eliminar tarea" name="btnEliminar">
-	</form>
+     <?php  } ?>
 
-	<p>..............................................................</p>
 
-	<form action="actualizar.php" method="POST">
-		Clave: <input type="text" name="txtClave"> <br/>
-		Tarea: <input type="text" name="txtTarea"> <br/>
-		Completado:
-			<select name="cmbCompletado">			
-				<option value="No">No</option>
-				<option value="Si">Si </option>
-			</select> <br/>
-		<input type="submit" value="Actualizar Tarea" name="btnActualizar">		
-	</form>
-<p>..............................................................</p>
+		<input type="text" name="task" class="task_input">
+		<button type="submit" class="add_btn" name="submit">add task</button>
+		</form>	
 
-<form action = "Registros.php" >
-	<input type="submit" value="Ver Registros" name="btnRegistros">
+		<table>
+			<thead>
+				<tr>
+					<th>N</th>
+					<th>Task</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+
+		<tbody>
+			<?php $i = 1; while ($row = mysql_fetch_array($tasks)) { ?>
+
+<tr>
+				<td><?php echo $i ; ?></td>
+				<td class="task"><?php echo $row['task']; ?></td>
+				<td class="delete">
+				    <a href="index.php?del_task=<?php echo $row['id']; ?>">X</a>
+				</td>
+			</tr>
+
+
+
+		<?php   $i++; }?>
+			
+
+		</tbody>
+
+		</table>
+
 	
-
-</form>
-
 
 
 </body>
